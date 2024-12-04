624fca59fd4a419708afee671ad04d35874919f8
bibek190 <bibek9818917036@gmail.com>
2024-12-04 11:11:34 +1100

    add shaders

---
+++ src/shaders/test/vertex.glsl
@@ -0,0 +1,19 @@
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+attribute vec3 position;
+
+void main(){
+vec4 modelPosition = modelMatrix * vec4(position,1.0);
+modelPosition.z += sin(modelPosition.x *30.0)*0.1;
+
+vec4 viewPosition = viewMatrix * modelPosition;
+vec4 projectedPosition = projectionMatrix * viewPosition;
+
+gl_Position = projectedPosition;
+
+
+  // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position,1.0);
+}
+  
